1d6023ff6b419109464432d184b9c0354a3663fd
Med Taha Hamdi <hamdi.taha@esprit.tn>
2025-01-20 02:04:14 +0100

    endpoint to the ErrorController for retrieving errors with filters

--- Domain/Interfaces/PagedResult.cs
+++ Domain/Interfaces/PagedResult.cs
@@ -2,5 +2,7 @@
 {
     public class PagedResult<T>
     {
+        public int TotalCount { get; set; }
+        public List<T> Items { get; set; }
     }
 }
--- Presentation/Controllers/ErrorController.cs
+++ Presentation/Controllers/ErrorController.cs
@@ -8,6 +8,7 @@ namespace Presentation.Controllers
     public class ErrorController : ControllerBase
     {
         //private readonly AssignErrorToEmployee _assignErrorToEmployee;
+        private readonly Ge _getFilteredErrors;
 
         //public ErrorController(AssignErrorToEmployee assignErrorToEmployee)
         //{
@@ -20,7 +21,23 @@ namespace Presentation.Controllers
             //await _assignErrorToEmployee.ExecuteAsync(request.ErrorId, request.EmployeeId);
             return Ok("Error assigned successfully.");
         }
+
+        [HttpGet("list")]
+        public async Task<IActionResult> GetFilteredErrors(
+        [FromQuery] DateTime? startDate,
+        [FromQuery] DateTime? endDate,
+        [FromQuery] string severity,
+        [FromQuery] int? assignedEmployeeId,
+        [FromQuery] int page = 1,
+        [FromQuery] int pageSize = 10)
+        {
+            var result = await _getFilteredErrors.ExecuteAsync(
+                startDate, endDate, severity, assignedEmployeeId, page, pageSize);
+
+            return Ok(result);
+        }
     }
+
 
     public class AssignErrorRequest
     {
